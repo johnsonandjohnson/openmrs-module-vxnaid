da2c31d934a3ba8aed02b54dc76e1c2a2139ffae
druchniewicz <druchniewicz@soldevelo.com>
2024-08-09 17:01:34 +0200

    VRVU-101: Added few new person attribute types

--- api/src/main/java/org/openmrs/module/vxnaid/api/metadata/InitialPersonAttributeTypesMetadata.java
+++ api/src/main/java/org/openmrs/module/vxnaid/api/metadata/InitialPersonAttributeTypesMetadata.java
@@ -10,31 +10,44 @@
 
 package org.openmrs.module.vxnaid.api.metadata;
 
-import org.openmrs.PersonAttributeType;import org.openmrs.api.PersonService;import org.openmrs.api.context.Context;import org.openmrs.module.metadatadeploy.bundle.VersionedMetadataBundle;
+import org.openmrs.PersonAttributeType;
+import org.openmrs.api.PersonService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.metadatadeploy.bundle.VersionedMetadataBundle;
 
 public class InitialPersonAttributeTypesMetadata extends VersionedMetadataBundle {
   @Override
   public int getVersion() {
-    return 1;
+    return 2;
   }
 
   @Override
   protected void installEveryTime() throws Exception {
-    //do nothing
+    // do nothing
   }
 
   @Override
   protected void installNewVersion() throws Exception {
-    createNewPersonAttributeType("Birth Weight", "4f9b97bf-4381-11ef-92cb-0242ac1d0002");
+    createNewPersonAttributeType(
+        "Birth Weight", "4f9b97bf-4381-11ef-92cb-0242ac1d0002", "Child weight at birth");
+    createNewPersonAttributeType(
+        "Mother's name", "8d871d18-c2cc-11de-8d13-0010c6dffd0f", "Child mother's name");
+    createNewPersonAttributeType(
+        "Child's name", "f1839002-417c-4b07-8da3-f58a36fffd25", "Child's name");
+    createNewPersonAttributeType(
+        "Father's name", "9104069d-2560-4d5a-b726-e4921c87e345", "Child father's name");
+    createNewPersonAttributeType(
+        "Child category", "d5e7bcbc-565c-11ef-9ee3-0242c0a89002", "Child's category");
   }
 
-  private void createNewPersonAttributeType(String name, String uuid) {
+  private void createNewPersonAttributeType(String name, String uuid, String description) {
     PersonService personService = Context.getPersonService();
 
     if (personService.getPersonAttributeTypeByName(name) == null) {
       PersonAttributeType personAttributeType = new PersonAttributeType();
       personAttributeType.setName(name);
       personAttributeType.setUuid(uuid);
+      personAttributeType.setDescription(description);
       personAttributeType.setFormat("java.lang.String");
       personService.savePersonAttributeType(personAttributeType);
     }
